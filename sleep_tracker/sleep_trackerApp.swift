9005a466eb8e1705e20d82e1f385e25faf55fe6f
bnicio <benicio@web.de>
2024-11-08 19:46:42 +0100

    Initial Commit

---
+++ sleep_tracker/sleep_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleep_trackerApp.swift
+//  sleep_tracker
+//
+//  Created by Benicio Nell on 08.11.24.
+//
+
+import SwiftUI
+
+@main
+struct sleep_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
